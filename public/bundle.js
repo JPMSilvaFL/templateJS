58f0c08d5ed5f40a912030e2e669dfb95cc9b397
JPMSilvaFL <joao.pedro.69461@gmail.com>
2025-01-30 01:30:13 -0300

    template

---
+++ public/bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+/*!*******************!*\
+  !*** ./server.js ***!
+  \*******************/
+var _element$index;
+(_element$index = element.index) !== null && _element$index !== void 0 ? _element$index : -1;
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
